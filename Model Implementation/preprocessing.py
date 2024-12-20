8f49813166cf79b0c245f1d1f19c0d9e19b5b4dc
Sara Hoxha <sarahoxha99@gmail.com>
2024-12-20 18:33:50 +0100

    updatet tensorflow to v 2.17.0 / keras to 3.7.0 / removed deprecated keras tokenizer  and used instead tf.keras.TextVectorization

--- Model Implementation/preprocessing.py
+++ Model Implementation/preprocessing.py
@@ -1,12 +1,14 @@
 import nltk
 nltk.download('punkt_tab')
 from nltk.tokenize import word_tokenize
+import tensorflow
 import numpy as np
 import contractions
 import re
-from keras.utils import pad_sequences
+from tensorflow.keras.utils import pad_sequences 
+from tensorflow.keras.layers import TextVectorization
 import contractions
-from keras.preprocessing.text import Tokenizer
+#from keras_preprocessing.text import Tokenizer
 def preprocess(text):
     """
     Preprocesses text by converting to lowercase and removing non-alphanumeric characters.
@@ -20,19 +22,18 @@ def preprocess(text):
 
 def tokenize_and_pad(train_data, max_length=100, num_words = 100000):
     """
-    Tokenizes and pads training data using Keras' Tokenizer.
+    Tokenizes and pads training data using TensorFlow.Keras Tokenizer.
     
     Args:
-        train_data: A list or pandas Series of text data for training.
+        train_data: A list or pandas Series of text data for TextVectorization.
         num_words: Maximum number of words to keep in the vocabulary.
         max_length: Maximum sequence length after padding/truncating.
 
     Returns:
         padded_sequences: Numpy array of tokenized and padded sequences.
-        tokenizer: The Keras Tokenizer fitted on the training data.
-        vocab_size: Size of the vocabulary (num_words + OOV + padding token).
+        tokenizer: The TextVectorization fitted on the training data.
+        vocab_size: Size of the vocabulary.
     """
-    UNK_TOKEN = "<unk>" 
     # Tokenize data
     tokenized_data = [word_tokenize(sentence.lower()) for sentence in train_data['text'].tolist()]
     
@@ -43,15 +44,19 @@ def tokenize_and_pad(train_data, max_length=100, num_words = 100000):
     max_length = int(np.percentile(tokenized_lengths, 95))
     
     # Initialize and fit tokenizer on training data
-    tokenizer = Tokenizer(num_words=num_words, oov_token=UNK_TOKEN)
-    tokenizer.fit_on_texts(train_data['text'])
+    text_vectorizer = TextVectorization(
+        max_tokens=num_words,
+        output_mode='int',
+        output_sequence_length=max_length
+    )   
+    text_vectorizer.adapt(train_data['text'])
 
     # Convert text to sequences
-    sequences = tokenizer.texts_to_sequences(train_data['text'])
+    vectorized_sequences = text_vectorizer(train_data['text'])
     
     # Pad sequences to the specified max_length
-    padded_sequences = pad_sequences(sequences, maxlen=max_length, padding="post", truncating="post")
+    padded_sequences = pad_sequences(vectorized_sequences, maxlen=max_length, padding="post", truncating="post")
     
-    vocab_size = len(tokenizer.word_index) + 1  # Add 1 for padding token
+    vocab_size = len(text_vectorizer.get_vocabulary())
 
-    return padded_sequences, tokenizer, vocab_size
+    return padded_sequences, text_vectorizer, vocab_size
